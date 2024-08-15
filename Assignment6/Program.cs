0bde4eb4b5fa747b7ecf3d8992ab516002f89eed
farid <farid@LAPTOP-SPTVSQVM>
2024-08-15 09:15:44 +0530

    Add project files.

---
+++ Assignment6/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Assignment6
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter the value for n:");
+        int n = int.Parse(Console.ReadLine());
+        if(n%2== 0)
+        {
+            Console.WriteLine($"{n} is a even number");
+        }
+        else
+        {
+            Console.WriteLine($"{n} is a odd number");
+        }
+    }
+}
